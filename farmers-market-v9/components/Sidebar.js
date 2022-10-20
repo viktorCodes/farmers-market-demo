3ad9d51304dad7810fe5ab823da18a42b661d60b
calimendoza <mbaivictor1@gmail.com>
2022-10-20 02:33:28 -0700

    start build process with sidebar component

---
+++ farmers-market-v9/components/Sidebar.js
@@ -0,0 +1,15 @@
+
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl: w-[340px]p-2 fixed h-full"  >
+
+        <div className="flex items-center justify-center w-13">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
